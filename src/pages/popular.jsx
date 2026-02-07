7dd3e80725aee3c8e30d94b96b111b6c4169aa38
Cloue M <senp41.mac@gmail.com>
2026-02-07 11:45:50 -0800

    Add popular.jsx

--- src/pages/popular.jsx
+++ src/pages/popular.jsx
@@ -1,7 +1,30 @@
+import { fetchPopularMovies, IMAGE_BASE_URL } from "../../service/services"
+import { useMovieData } from "../hooks/useMovieData"
+import Loading from "../components/Loading"
+import GenreRow from "../components/genre-row"
+import "../styles/category-page.css"
+
 export default function Popular() {
+    const { moviesByGenre, loading, error } = useMovieData(fetchPopularMovies);
+
+    if (loading) {
+        return <Loading />;
+    }
+
+    if (error) {
+        return <div style={{ padding: '20px', color: 'red' }}><h1>Popular</h1><p>Error: {error}</p></div>;
+    }
+
     return (
-        <div>
-            <p>The content of the Popular page goes here.</p>
+        <div className="now-playing-page">
+            {Object.entries(moviesByGenre).map(([genreName, movies]) => (
+                <GenreRow
+                    key={genreName}
+                    genreName={genreName}
+                    movies={movies}
+                    IMAGE_BASE_URL={IMAGE_BASE_URL}
+                />
+            ))}
         </div>
     );
-}
+};
