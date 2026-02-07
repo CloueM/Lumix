b731db947f03bcdd80b58888381467da569bf4b2
Cloue M <senp41.mac@gmail.com>
2026-02-07 11:56:04 -0800

    Added Upcoming.jsx

--- src/pages/upcoming.jsx
+++ src/pages/upcoming.jsx
@@ -1,6 +1,30 @@
+import { fetchUpcomingMovies, IMAGE_BASE_URL } from "../../service/services"
+import { useMovieData } from "../hooks/useMovieData"
+import Loading from "../components/Loading"
+import GenreRow from "../components/genre-row"
+import "../styles/category-page.css"
+
 export default function Upcoming() {
+    const { moviesByGenre, loading, error } = useMovieData(fetchUpcomingMovies);
+
+    if (loading) {
+        return <Loading />;
+    }
+
+    if (error) {
+        return <div style={{ padding: '20px', color: 'red' }}><h1>Upcoming</h1><p>Error: {error}</p></div>;
+    }
+
     return (
-        <div>
+        <div className="upcoming-page">
+            {Object.entries(moviesByGenre).map(([genreName, movies]) => (
+                <GenreRow
+                    key={genreName}
+                    genreName={genreName}
+                    movies={movies}
+                    IMAGE_BASE_URL={IMAGE_BASE_URL}
+                />
+            ))}
         </div>
     );
-}
+};
